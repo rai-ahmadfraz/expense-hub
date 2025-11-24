f1f4ad303bad88e152dca769746fe496c723e1ad
Rai Ahmad Fraz <raiahmadfraz@gmail.com>
2025-11-24 17:32:21 +0500

    add btn added

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -20,14 +20,30 @@ const BottomMenu = () => {
           <Link key={item.name} href={item.link}>
             <button
               onClick={() => setActive(item.name)}
-              className={`flex-1 text-center py-2 text-sm font-medium transition-colors duration-200 ${
-                active === item.name ? "text-blue-500 border-t-2 border-blue-500" : "text-gray-500"
-              }`}
+              className={`flex-1 text-center py-2 text-sm font-medium transition-colors duration-200
+                ${active === item.name ? "text-blue-500 border-t-2 border-blue-500" : "text-gray-500"}
+              `}
             >
-              {item.name}
+              {item.name == "Add" ? "":item.name}
+             {item.name === "Add" &&
+              <svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  strokeWidth="1"
+  className="size-10"
+>
+  <rect x="4" y="4" width="16" height="16" rx="5" ry="5" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v8m-4-4h8" />
+</svg>
+
+             } 
             </button>
           </Link>
         ))}
+        <button>
+        </button>
       </div>
     </div>
   );
